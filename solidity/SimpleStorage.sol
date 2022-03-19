11d920cd08ac50a3bc7a4f233036045d1097d88a
Saifur Rehman Khan <saifurrahmankhankhan@gmail.com>
2022-03-19 23:44:10 +0530

    first commit

---
+++ solidity/SimpleStorage.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+// contracts are like class keyword of Java
+contract SimpleStorage {
+    // dataypes
+    uint256 private x = 5;
+    bool isOk = true;
+    string name = "saifu";
+    bytes4 name2 = "ssas";
+    address myAddress = 0xfca28D89A7507130469275F7370D0bB380fEB7e7;
+    int32 y = -5;
+
+    // methods/functions
+    // function store(uint256 _x) public {
+    //     x = _x;
+    // }
+
+    // // view method
+    // function retrieve() public view returns(uint256) {
+    //     return x;
+    // }
+
+    // // pure method => only for math computation
+    // function doMath(uint256 _x) public pure {
+    //     _x + _x;
+    // }
+
+    // struct
+    struct People {
+        string name;
+        int32 age;
+    }
+
+    // People public saif = People({name: "Saif-ur-Rahman", age: 22});
+
+    // arrays
+    People[] public people;
+    function addPerson(string memory _name, int32 _age) public {
+        people.push(People(_name, _age));
+    }
+}
